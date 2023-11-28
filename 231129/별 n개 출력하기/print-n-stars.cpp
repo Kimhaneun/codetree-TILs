8600646a3d23e4e3e8b1dbd4fc1468d05a33ed3c
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-11-29 08:44:59 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a, n = 1;
+    cin >> n;
+    while (a < n)
+    {
+        cout << "*" << endl;
+        a++;
+    }
+    return 0;
+}
