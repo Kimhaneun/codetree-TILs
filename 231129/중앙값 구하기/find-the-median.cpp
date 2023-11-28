e6ad3bd48311b7f163a3b24478e00ea1612d294f
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-11-29 00:20:57 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/중앙값 구하기/find-the-median.cpp
+++ 231129/중앙값 구하기/find-the-median.cpp
@@ -5,10 +5,10 @@ int main() {
     int a, b, c;
     cin >> a >> b >> c;
 
-    if (a > b > c || c > b > a)
+    if ((a > b && b > c) || (c > b && b > a))
     cout << b;
 
-     if (b > c > a || a > c > b)
+    else if ((b > c && c > a) || (a > c && c > b))
     cout << c;
 
     else 
