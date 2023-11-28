94f65848f4ce9f8c058fbc0d530d9bacca273c44
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-11-28 18:54:28 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231128/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int am, ae, bm, be;
+    cin >> am >> ae;
+    cin >> bm >> be;
+    if (am == bm){
+        ae > be ? cout << "A" : cout << "B";
+    }
+    else 
+    am > bm ? cout << "A" : cout << "B";
+
+    return 0;
+}
