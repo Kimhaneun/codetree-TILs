a924c816c654e06559990e2733746d77b2b039d3
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-11-29 11:36:16 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    
+    for (int i = 0; i < 8; i++)
+    {
+        cout << a;
+    }
+
+
+    return 0;
+}
