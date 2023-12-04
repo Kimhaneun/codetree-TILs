a4e76d1ec7758931b7a425869b25d9737b3303ab
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-12-04 18:54:47 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231204/숫자의 증감/increasing-and-decreasing-numbers.cpp
+++ 231204/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -5,18 +5,18 @@ int main() {
     char c;
     int n;
     cin >> c >> n;
-    if (c == 'A')
+    if (c == "A")
     {
         for (int i = 1; i <= n; i++)
         {
             cout << i << " ";
         }
     }
-    else if (c == 'D')
+    else if (c == "D")
     {
-        for (int i = n; i >= n; i--)
+        for (int i = n; i <= n; i--)
         {
-          cout << i << " ";
+            cout << i << " ";
         }
     }
     return 0;
