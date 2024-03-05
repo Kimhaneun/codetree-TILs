d486198fc39701005a29b65da3fada9c68c459e5
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-05 23:41:36 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240305/a의 b승/a-to-the-power-of-b.cpp
+++ 240305/a의 b승/a-to-the-power-of-b.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 int main() {
     int a, b = 0;
     cin >> a >> b;
 
-    cout << pow(a, b);
+    int sum = a;
+    for (int i = a; i <= b; i++)
+    {
+        sum *= a;
+    }
+
+    cout << sum;
     return 0;
 }
