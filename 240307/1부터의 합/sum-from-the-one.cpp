9c551fb9d8e5a8666e7809da5c9cf273946d5bd3
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-07 08:30:20 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240307/1부터의 합/sum-from-the-one.cpp
+++ 240307/1부터의 합/sum-from-the-one.cpp
@@ -4,20 +4,14 @@ using namespace std;
 int main() {
     int n, answer = 0;
     cin >> n;
+    //  곱하다가 최초로 5의 배수가 되는 순간에 곱하는 것을 멈추고 해당 값을 출력한 뒤 끝내는 프로그램
 
     for(int i = 1; i <= 100; i++){
-
-        if(answer + i > n){
-            cout << answer;
-                    break;
-        }
-        answer += i;
-        if(answer + i >= 100){
-            answer = 100;
-            cout << answer;
+        if(answer + i > n)
             break;
-        }
-
+        answer += i;
     }
+    cout << answer;
+
     return 0;
 }
