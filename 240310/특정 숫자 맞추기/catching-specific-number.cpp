3d3a7b31585eb3bbf026dd7c6996f567e99b765b
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-10 21:12:38 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int n;
+
+    while(true){
+        std::cin >> n;
+        if(n < 25){
+            std::cout << "Higher\n";
+        }
+        else if(n > 25){
+            std::cout << "Lower\n";
+        }
+        else if(n == 25){
+            std::cout << "Good\n";
+            break;
+        }
+    }
+    return 0;
+}
