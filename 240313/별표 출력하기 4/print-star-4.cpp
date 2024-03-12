9749b2835b83bb59061635db184c91f18ab3e82d
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-13 00:13:13 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int n;
+
+    std :: cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        for(int j = n; i < j; j--){
+            std :: cout << "*" << " ";
+        }
+        std :: cout << "\n";
+    }
+
+    for(int i = 2; i <= n; i++){
+        for(int j = 0; j < i; j++){
+            std :: cout << "*" << " ";
+        }
+        std :: cout << "\n";
+    }
+
+    return 0;
+}
