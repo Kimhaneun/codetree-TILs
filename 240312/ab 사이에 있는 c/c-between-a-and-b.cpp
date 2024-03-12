2ee7dbcbd027c4bbba9122913e0f6966d39f48c2
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-12 18:26:13 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/ab 사이에 있는 c/c-between-a-and-b.cpp
+++ 240312/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -2,20 +2,21 @@
 
 int main() {
     int a, b, c;
-    bool isBool;
+    bool isBool = false;
 
     std :: cin >> a >> b >> c;
     
     for(int i = a; i <= b; i++){
         if(i % c == 0){
             isBool = true;
+            break;
         }
     }
 
     if(isBool)
         std :: cout << "YES";
 
-    else
+    else 
         std :: cout << "NO";
 
     return 0;
