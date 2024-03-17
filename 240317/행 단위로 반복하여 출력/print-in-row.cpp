bbe8374c58878e38b73c3914e23a1b50f7ad1a20
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-03-17 23:29:46 +0900

    Title: 행 단위로 반복하여 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240317/행 단위로 반복하여 출력/print-in-row.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+
+    int n;
+
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            std::cout << j + 1;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
