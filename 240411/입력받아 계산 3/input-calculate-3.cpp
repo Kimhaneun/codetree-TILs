7b81644c87f3c3624ea5a0582ee63cee12828f20
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2024-04-11 18:23:25 +0900

    Title: 입력받아 계산 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/입력받아 계산 3/input-calculate-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+
+    int a, b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << a * b;
+    return 0;
+}
